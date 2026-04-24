82052a49ef8e5ee42309efcc5118e6364f5c8275
Himanshu <masanthimanshu@gmail.com>
2026-04-24 12:44:16 +0530

    updates

--- core/dynamo_client.js
+++ core/dynamo_client.js
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const dbClient = new DynamoDBClient({ region: process.env.AWS_REGION });
+const dbClient = new DynamoDBClient({ region: process.env.CURRENT_AWS_REGION });
 const docClient = DynamoDBDocumentClient.from(dbClient);
 
 export default docClient;
